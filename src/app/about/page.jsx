2514cccaf01cdb1eacd98b74c52253f3ac3eda35
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-05-25 23:26:27 -0400

    Finished About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,12 +3,39 @@ import Image from "next/image";
 
 const AboutPage = () => {
 	return (
-		<div>
+		<div className={styles.container}>
+			<div className={styles.textContainer}>
+				<h2 className={styles.subtitle}>About Agency</h2>
+				<h1 className={styles.title}>
+					We create digital ideas that are bigger, bolder, braver and
+					better.
+				</h1>
+				<p className={styles.desc}>
+					We create digital ideas that are bigger, bolder, braver and
+					better. We believe in good ideas flexibility and precission We’re
+					world's Our Special Team best consulting & finance solution
+					provider. Wide range of web and software development services.
+				</p>
+				<div className={styles.boxes}>
+					<div className={styles.box}>
+						<h1>10 K</h1>
+						<p>Year of experience</p>
+					</div>
+					<div className={styles.box}>
+						<h1>10 K</h1>
+						<p>Year of experience</p>
+					</div>
+					<div className={styles.box}>
+						<h1>10 K</h1>
+						<p>Year of experience</p>
+					</div>
+				</div>
+			</div>
 			<div className={styles.imageContainer}>
 				<Image
-					src="https://images.pexels.com/photos/24838633/pexels-photo-24838633/free-photo-of-the-fernsehturm-tower-in-berlin-germany.jpeg"
-					alt=""
+					src="/about.png"
 					fill
+					className={styles.img}
 				/>
 			</div>
 		</div>
